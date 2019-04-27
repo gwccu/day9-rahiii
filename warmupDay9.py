4b77853678d5b84382665cf1cab5aac88c07c7b3
rahiii <rahimitranyc@gmail.com>
2019-04-27 15:24:37 +0000

    Warmup

--- warmupDay9.py
+++ warmupDay9.py
@@ -1,1 +1,19 @@
 # File name: warmupDay9.py
+ages = [15,30,25,14]
+a = 1
+for i in range(len(ages)):
+  a = a*ages[i]
+print(a)
+
+max = 0
+for i in ages:
+  if i > max:
+    max = i
+print(max)
+
+a = i
+for i in range(len(ages)):
+  a = i
+print(a + 1)
+
+print(ages)
