f352097bfc4c19f95bcda071e347169914043687
rahiii <rahimitranyc@gmail.com>
2019-05-04 16:15:42 +0000

    Hangman

--- problemSetDay9.py
+++ problemSetDay9.py
@@ -1,18 +1,44 @@
 # File name: problemSetDay9.py
 import random
 Hangman_words = ["hat", "happy", "slides", "charger", "mice", "phone"]
-colors_length = len(Hangman_words)
-random_index = random.randint(0,colors_length-1)
+word_length = len(Hangman_words)
+random_index = random.randint(0, word_length-1)
 word = Hangman_words[random_index]
-'''print(list(word))'''
 word_length = len(word)
+
 print("lets play hangman!Your word has", len(word), "letters")
-blank = "_ " * len(word)
-print(blank)
+#blank = "_ " * len(word)
+#print(blank)
 wrong = []
-guess = input("Guess a letter")
-if guess in word:
-  print("alight")
-else:
-  wrong.append(guess)
-  print(wrong)
+guesses_left = 10
+blank = []
+
+for i in range(len(word)):
+    blank.append("_")
+print(blank)
+
+while guesses_left > 0 and "_" in blank:
+    user_guess = raw_input("Guess a letter ")
+    if user_guess in word:
+        for i in range(len(word)):
+            if user_guess == word[i]:
+                blank[i] = user_guess
+    elif user_guess in wrong:
+        print("You already guessed that letter")
+    else:
+        wrong.append(user_guess)
+        guesses_left -= 1
+    print(wrong)
+    print(guesses_left)
+    print(blank)
+
+
+
+
+'''
+  if guess in word:
+    print("alight")
+  else:
+    wrong.append(guess)
+    print(wrong)
+'''
